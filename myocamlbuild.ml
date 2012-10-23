94aed05b4098354df640bd43ba2fe00340555914
Hezekiah M. Carty <hez@0ok.org>
2012-10-22 20:20:03 -0400

    Detect PLplot compilation and linking flags using pkg-config

---
+++ myocamlbuild.ml
@@ -0,0 +1,54 @@
+(* OASIS_START *)
+(* OASIS_STOP *)
+
+let () =
+  let additional_rules = function
+    | After_rules ->
+        (* Add correct PLplot compilation and link flags *)
+        let plplot_clibs, oplplot_cflags, oplplot_clibs =
+          let icc = Unix.open_process_in "pkg-config plplotd --cflags" in
+          let icl = Unix.open_process_in "pkg-config plplotd --libs" in
+          try
+            let plplot_cflags = input_line icc in
+            let plplot_clibs = input_line icl in
+            (* TODO: remove once split-function in generated code is fixed *)
+            let rec split_string s =
+              match try Some (String.index s ' ') with Not_found -> None with
+              | Some pos ->
+                  let sub = String.before s pos in
+                  if sub <> "" then
+                    sub :: split_string (String.after s (pos + 1))
+                  else
+                    split_string (String.after s (pos + 1))
+              | None -> if s <> "" then [s] else []
+            in
+            let ocamlify ~ocaml_flag flags =
+              let chunks = split_string flags in
+              let cnv flag = [A ocaml_flag; A flag] in
+              List.concat (List.map cnv chunks)
+            in
+            let split_flags flags =
+              let chunks = split_string flags in
+              let cnv flag = A flag in
+              List.map cnv chunks
+            in
+            close_in icl;
+            close_in icc;
+            S (split_flags plplot_clibs),
+            S (ocamlify ~ocaml_flag:"-ccopt" plplot_cflags),
+            S (ocamlify ~ocaml_flag:"-cclib" plplot_clibs)
+          with exn ->
+            close_in icl;
+            close_in icc;
+            raise exn
+        in
+        flag ["compile"; "c"] oplplot_cflags;
+        flag ["link"; "ocaml"; "library"] oplplot_clibs;
+        flag ["oasis_library_plplot_cclib"; "ocamlmklib"; "c"] plplot_clibs;
+        flag ["oasis_library_plplot_cclib"; "link"] oplplot_clibs
+      | _ -> ()
+  in
+  dispatch (
+    MyOCamlbuildBase.dispatch_combine
+      [MyOCamlbuildBase.dispatch_default package_default; additional_rules])
+
